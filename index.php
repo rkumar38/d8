a3c26fd7de731762a783fd38a1e4a5f7f99311a2
Ramesh Kumar <38499777+rkumar38@users.noreply.github.com>
2018-12-03 15:24:48 +0530

    Update index.php
    cm

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 /**
  * @file
  * The PHP page that serves all page requests on a Drupal installation.
- *
+ * Azure VM conf
  * All Drupal code is released under the GNU General Public License.
  * See COPYRIGHT.txt and LICENSE.txt files in the "core" directory.
  */
